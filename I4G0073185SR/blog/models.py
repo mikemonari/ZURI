85d5aad5abb4ec8a01959a05a605c979a270ccd2
carasa <44632005+carasa@users.noreply.github.com>
2022-06-14 13:48:50 +0300

    'blog'

--- I4G0073185SR/blog/models.py
+++ I4G0073185SR/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post{models.Model}:
+    Title = models.CharField(maxlength = 200)
+    Text = models.TextField()
+    Author = models.ForeignKey(get_user_model())
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
